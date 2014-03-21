5555927401413567e49e659ea2a8eb93e261a712
brownaep <brownaep@bc.edu>
2014-03-21 16:36:19 -0400

    PLEASE
    REnamed FrequencyEntry.java and FrequencyEntryC.java to SymbolTable.java
    and SymbolTableC.java. Created HuffTree.java and HuffTreeC.java. Added
    new stuff

---
+++ HuffTree.java
@@ -0,0 +1,11 @@
+// file: HuffTree.java
+// author: Matthew Brown & Ryan Lee
+// date: 3/28/14
+//
+// ADT for a weighted binary tree used in Huffman encoding
+//
+public interface HuffTree
+{
+    public int size();
+    public String toString();
+}
---
+++ HuffTreeC.java
@@ -0,0 +1,34 @@
+// file: HuffTreeC.java
+// author: Matthew Brown & Ryan Lee
+// date: 3/28/14
+//
+// Implementation of an ADT for a weighted binary tree used in Huffman encoding
+//
+public class HuffTreeC
+{
+    private int size;
+    private Node root;
+    
+    private class Node
+    {
+        private Node left;
+        private Node right;
+        private int weight;
+    //  private [something] symbol;
+        
+        private Node(int weight, symbol, Node left, Node right)
+        {
+            this.weight = weight;
+            this.symbol = symbol;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public HuffTreeC()
+    {
+        
+    }
+    public int size();
+    public String toString();
+}
