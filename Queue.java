42e0ba32ce1a89de3da50929fd0db34a106e2574
RyanChenLee <ryanlee2012@gmail.com>
2014-03-21 16:08:01 -0400

    Delete Queue

--- Queue.java
+++
@@ -1,6 +0,0 @@
-public interface Queue<T> {
-    public void enqueue(T info); //info to put into the queue; "VOID" b/c it doesn't return anything! 
-    public dequeue();
-    public Boolean isEmpty(); //to tell if empty
-    public String toString();
-}
